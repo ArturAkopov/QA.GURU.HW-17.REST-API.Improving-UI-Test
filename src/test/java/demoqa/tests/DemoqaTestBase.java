5a2d5f25940a0153cbd5c59826b6aabdd20cf704
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-06-15 00:38:21 +0300

    Добавлены конфиги и переход на браузерную ферму Selenoid

--- src/test/java/demoqa/tests/DemoqaTestBase.java
+++ src/test/java/demoqa/tests/DemoqaTestBase.java
@@ -1,9 +1,15 @@
 package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -11,14 +17,31 @@ public class DemoqaTestBase {
 
     @BeforeAll
     static void setup() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("size", "1920x1080");
+        Configuration.browserVersion = System.getProperty("version","121");
+
+        Configuration.remote = "https://user1:1234@"+System.getProperty("selenoid","selenoid.autotests.cloud")+"/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
 
         RestAssured.baseURI = "https://demoqa.com";
     }
 
     @AfterEach
-    void shutDown() {
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
 }
